4ce52e116a8aadcec5f7e41f660bc5f52b6c9ba2
Willem Carel de Jongh <willemcarel@gmail.com>
2025-10-15 07:13:24 +0200

    feat: Multi tables per DB id

--- cookbook/demo/run.py
+++ cookbook/demo/run.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 
-from agents.agno_assist import agno_assist
+from agents.agno_assist import agno_assist, agno_assist2
 from agents.web_search import web_search_agent
 from agno.os import AgentOS
 from teams.multilingual_team import multilingual_team
